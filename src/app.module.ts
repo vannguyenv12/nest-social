41743d657fbfc8085ace310e419804e720fc14af
vannguyenv12 <npnv.vn1@gmail.com>
2025-04-26 16:33:27 +0700

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://vannguyenv12:vannguyenv12@socialcluster.nwn6fft.mongodb.net/social_db?retryWrites=true&w=majority&appName=SocialCluster',
-    ),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     UserModule,
   ],
   controllers: [AppController],
