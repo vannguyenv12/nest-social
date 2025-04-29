18923dd1660e3aeff2c2587d015c90a802e4abca
vannguyenv12 <npnv.vn1@gmail.com>
2025-04-29 21:55:11 +0700

    auth guard - part 2

--- src/_cores/guards/role.guard.ts
+++ src/_cores/guards/role.guard.ts
@@ -1,7 +1,13 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { ROLES_KEY } from '../decorators/role.decorator';
+import { Request } from 'express';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
@@ -10,12 +16,34 @@ export class RoleGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<Request>();
+    const currentUser = request.currentUser;
 
-    const requiredRoles = this.reflector.get(ROLES_KEY, context.getHandler());
+    const requiredRoles = this.reflector.get(
+      ROLES_KEY,
+      context.getHandler(),
+    ) as IRole[];
 
-    console.log(requiredRoles);
+    if (!requiredRoles) {
+      return true;
+    }
 
-    return true;
+    if (requiredRoles.length === 0) {
+      return true;
+    }
+
+    if (requiredRoles.includes('admin') && currentUser.role === 'admin') {
+      return true;
+    }
+
+    if (requiredRoles.includes('user') && currentUser.role === 'user') {
+      const userId = currentUser._id;
+      const resourceId = request.params.id;
+
+      if (userId === resourceId) return true;
+      throw new ForbiddenException('You can only access your own resources');
+    }
+
+    throw new ForbiddenException('You do not have enough permission');
   }
 }
