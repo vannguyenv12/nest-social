3463f6511d9d9b5b1b1ddc6d5c228fb2e53412fe
vannguyenv12 <npnv.vn1@gmail.com>
2025-05-01 09:52:32 +0700

    comment schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { PostModule } from './post/post.module';
 import { ResourceModule } from './resource/resource.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { ReactionModule } from './reaction/reaction.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { ReactionModule } from './reaction/reaction.module';
     ResourceModule,
     CloudinaryModule,
     ReactionModule,
+    CommentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/comment/schemas/comment.entity.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { PostDocument } from 'src/post/schemas/post.schema';
+import { UserDocument } from 'src/user/schemas/user.schema';
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema({ timestamps: true })
+export class Comment {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Post' })
+  post: PostDocument;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' })
+  parent: CommentDocument | null;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  replyToUser: UserDocument | null;
+
+  @Prop({ required: true })
+  content: string;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
